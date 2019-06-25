ed0d0e3f8e0d89b551f9b87c53bb09bbebc6f9e3
Tom Becker <tjbecker@me.com>
2019-06-25 12:21:53 +0700

    trying to figure out controllers

--- controllers/serch_get.js
+++ controllers/serch_get.js
@@ -2,15 +2,28 @@ const db = require('../db.js')
 
 
 module.exports = (req, res) => {
-	console.log('req query', req.query);
-	if (req.query.name) {
-		query += `WHERE name = ${req.query.name}`
+	let query = `SELECT * FROM properties ` // default query = all products
+	if (req.query && Object.keys(req.query).length) { // add WHERE if request has query
+		query += `WHERE `
 	}
-	db.query(query,(err, result) => {
+	if (req.query.name) {
+		query += `name = '${req.query.name}' `
+	}
+	if (req.query.price) {
+		query += `price = '${req.query.price}' `
+	}
+	if (req.query.rating) {
+		query += `rating = '${req.query.rating}' `
+	}
+	if (req.query.rooms) {
+		query += `rooms = '${req.query.room}' `
+	}
+	
+	console.log(query);
+	db.query(query, (err, result) => {
 		if (err) {
 			res.send(err)
-		}
-		else {
+		} else {
 			res.send(result.rows)
 		}
 	})
