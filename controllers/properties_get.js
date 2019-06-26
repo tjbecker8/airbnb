050e7517fd8cfd0c7089e9e9f1f368348ce9e94b
Tom Becker <tjbecker@me.com>
2019-06-26 12:45:03 +0700

    update controller to give right info

--- controllers/properties_get.js
+++ controllers/properties_get.js
@@ -1,7 +1,12 @@
 const db = require('../db.js')
 
 module.exports = (req, res) => {
-	db.query(`SELECT * FROM properties`, (err, result) => {
+	db.query(`SELECT properties.id, properties.name, properties.price, properties.rating, properties.rooms, properties.country, properties.image, proptype.type AS "type", cities.name AS "city"
+FROM properties
+LEFT JOIN proptype
+	ON properties.type = proptype.id
+LEFT JOIN cities
+	ON properties.city = cities.id`, (err, result) => {
 		if (err) {
 			console.log('err', err)
 		} else {
