d056d86c7c951c8d8e2b23c0919dc6ad3ef0a0d9
Tom Becker <tjbecker@me.com>
2019-06-28 12:10:54 +0700

    adding basic compenets for adding property

--- client/js/admin.js
+++ client/js/admin.js
@@ -1,8 +1,36 @@
-onsole.log('admin.js is connected');
+console.log('admin.js is connected');
 
 window.onload = () => {
 
+	axios.get('/api/cities').then((res) => {
+		let city = res.data
+		let city_ul = document.getElementById('cities-ol')
+		city.forEach((c)=> {
+			city_ul.insertAdjacentHTML('beforeEnd', `
+			<li>${c.name}</li>
+			`)
+		})
+	})
 
 
+	axios.get('/api/countries').then((res) => {
+		let country = res.data
+		let country_ul = document.getElementById('count-ol')
+		country.forEach((c)=> {
+			country_ul.insertAdjacentHTML('beforeEnd', `
+			<li>${c.name}</li>
+			`)
+		})
+	})
+
+	axios.get('/api/roomtype').then((res) => {
+		let type = res.data
+		let type_ul = document.getElementById('type-ol')
+		type.forEach((t)=> {
+			type_ul.insertAdjacentHTML('beforeEnd', `
+			<li>${t.type}</li>
+			`)
+		})
+	})
 
 }
