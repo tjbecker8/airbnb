08c6e1eacb4772cf71d58921472c175a5a465d02
Tom Becker <tjbecker@me.com>
2019-06-30 16:30:28 +0700

    fixed controllers

--- controllers/properties_get.js
+++ controllers/properties_get.js
@@ -1,14 +1,33 @@
 const db = require('../db.js')
 
+
 module.exports = (req, res) => {
-	db.query(`SELECT properties.id, properties.name, properties.price, properties.rating, properties.rooms, properties.country, properties.image, proptype.type AS "type", cities.name AS "city"
-						FROM properties
-						LEFT JOIN proptype
-						ON properties.type = proptype.id
-						LEFT JOIN cities
-						ON properties.city = cities.id`, (err, result) => {
+	let query = `SELECT properties.id, properties.name, properties.price, properties.rating, properties.rooms, properties.image, cities.name AS "city", proptype.type AS "type"
+								FROM properties
+								LEFT JOIN cities
+								ON properties.city = cities.id
+								LEFT JOIN proptype
+	 							ON properties.type = proptype.id ` 
+	if (req.query && Object.keys(req.query).length) { // add WHERE if request has query
+		query += `WHERE `
+	}
+	if (req.query.name) {
+		query += `name = '${req.query.name}' `
+	}
+	if (req.query.price) {
+		query += `price = '${req.query.price}' `
+	}
+	if (req.query.rating) {
+		query += `rating = '${req.query.rating}' `
+	}
+	if (req.query.rooms) {
+		query += `rooms = '${req.query.room}' `
+	}
+
+	// console.log(query);
+	db.query(query, (err, result) => {
 		if (err) {
-			console.log('err', err)
+			res.send(err)
 		} else {
 			res.send(result.rows)
 		}
